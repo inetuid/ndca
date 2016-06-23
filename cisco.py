0afc78575d98bbc482fd653d100e3a750bf75aec
Matt Ryan <inetuid@gmail.com>
2016-06-23 19:14:21 +0100

    Tested against Cisco router.

--- cisco.py
+++ cisco.py
@@ -1,6 +1,12 @@
 import re
 import yandc
 
+def ios_version(s):
+	re_match = re.match(r'Cisco IOS Software, .+, Version ([^\,]+),', s)
+	if re_match is not None:
+		return re_match.groups()[0]
+	return None
+
 class IOS_Client(yandc.vendor_base.Client):
 	def __enter__(self):
 		return self
@@ -14,73 +20,87 @@ class IOS_Client(yandc.vendor_base.Client):
 		grouped_kwargs = self.group_kwargs('snmp_', 'ssh_', **kwargs)
 
 		if 'snmp_' in grouped_kwargs:
-			snmp_client_ = SNMP_Client(kwargs['host'], **grouped_kwargs['snmp_'])
+			snmp_client = SNMP_Client(kwargs['host'], **grouped_kwargs['snmp_'])
 
 			try:
-				if snmp_client_.enterprise()[0] != 'Cisco':
+				if not snmp_client.sysObjectID().startswith('1.3.6.1.4.1.9.1'):
 					raise ValueError('Not a Cisco device')
-			except snmp.GetError:
+			except yandc.snmp.GetError:
 				pass
 			else:
-				self.snmp_client = snmp_client_
+				self.snmp_client = snmp_client
 
 		if 'ssh_' in grouped_kwargs:
 			self.ssh_client = SSH_Client(kwargs['host'], **grouped_kwargs['ssh_'])
-			shell_prompt = None
-			self.shell_channel = self.ssh_client.shell(prompt=shell_prompt)
+
+			shell_prompt = yandc.ssh.ShellPrompt(yandc.ssh.ShellPrompt.regexp_prompt(r'.+[#>]$'))
+			shell_prompt.add_prompt(yandc.ssh.ShellPrompt.regexp_prompt(r'.+\(config[^\)]*\)#$'))
+
+			self.shell = SSH_Shell(self.ssh_client, shell_prompt)
+			self.shell.channel.set_combine_stderr(True)
+			self.shell.command('terminal length 0')
+			self.shell.command('terminal no monitor')
+			self.shell.command('terminal width 160')
+
 
 	def cli_command(self, *args, **kwargs):
-		return self.ssh_command(*args, **kwargs)
+		if self.can_ssh():
+			return self.ssh_command(*args, **kwargs)
+		raise Exception('No valid CLI handlers')
 
 	def configure_via_cli(self, new_config):
-		try:
-			cli_output = self.ssh_command('configure terminal')
-			if cli_output == []:
-				self.in_configure_mode(True)
-			else:
-				raise ValueError(cli_output[0])
-
-			for config_line in new_config:
-				stripped_line = config_line.lstrip().rstrip()
-
-				if stripped_line == 'end' or stripped_line == 'exit':
-					continue
-
-				cli_output = self.ssh_command(stripped_line)
-				if cli_output != []:
+		if self.can_ssh():
+			try:
+				cli_output = self.ssh_command('configure terminal')
+				if not cli_output[0].startswith("'Enter configuration commands, one per line."):
+					self.in_configure_mode(True)
+				else:
 					raise ValueError(cli_output[0])
-		finally:
-			cli_output = self.ssh_command('end')
-			if cli_output == []:
-				self.in_configure_mode(False)
 
-				return True
-			else:
-				raise ValueError(cli_output[0])
+				for config_line in new_config:
+					stripped_line = config_line.lstrip().rstrip()
 
+					if stripped_line in ['end', 'exit']:
+						continue
+
+					cli_output = self.ssh_command(stripped_line)
+					if cli_output != []:
+						raise ValueError(cli_output[0])
+			finally:
+				cli_output = self.ssh_command('end')
+				if cli_output == []:
+					self.in_configure_mode(False)
+
+					return True
+				else:
+					raise ValueError(cli_output[0])
 		return False
 
 	def disconnect(self):
-		if self.can_ssh() and hasattr(self, 'shell_channel'):
-			self.ssh_client.shell_exit(self.shell_channel, 'logout')
-			self.shell_channel.close()
-			del self.shell_channel
+		if self.can_ssh() and hasattr(self, 'shell'):
+			self.shell.exit()
+			del self.shell
 
 		super(IOS_Client, self).disconnect()
 
+		if hasattr(self, '_in_configure_flag'):
+			del self._in_configure_flag
 		if hasattr(self, '_software_version'):
 			del self._software_version
 
-	def get_config(self, config_source='running', config_filter=None):
-		return self.ssh_command('show ' + config_source + '-config')
+	def get_config(self, source='running', section=None):
+		if self.can_ssh():
+			config_command = 'show {}-config'.format(source)
+			if section is not None:
+				config_command += ' | section {}'.format(section)
+			return self.ssh_command(config_command)
+		return []
 
-	def get_interface_config(self, if_name):
-		return self.ssh_command('show running-config interfaces ' + if_name)
+	def in_configure_mode(self, config_mode=None):
+		if config_mode is not None:
+			self._in_configure_flag = config_mode
 
-	def in_configure_mode(self, *args, **kwargs):
-		config_mode = super(Client, self).in_configure_mode(*args, **kwargs)
-
-		last_prompt = self.ssh_client.shell_last_prompt(self.shell_channel)
+		last_prompt = self.shell.last_prompt()
 		prompt_length = len(last_prompt)
 
 		if prompt_length > 9:
@@ -93,40 +113,40 @@ class IOS_Client(yandc.vendor_base.Client):
 				if prompt_part == '(config)#':
 					raise ValueError('Mistmatch between in_configure_mode(' + str(config_mode) + ') and prompt [' + last_prompt + ']')
 
-		return config_mode
+		return getattr(self, '_in_configure_flag', False)
 		
 	def persist_configuration(self):
  		if self.in_configure_mode():
  			pass
  
- 		cli_output = self.ssh_command('copy running-config startup-config')
- 		if cli_output[0] != 'Copy completed successfully.':
-			pass
-# 			raise ValueError(cli_output[0])
+ 		cli_output = self.ssh_command('write memory')
+ 		if cli_output[0] != 'Building configuration...' and cli_output[-1] != '[OK]':
+ 			raise ValueError(cli_output[0])
  
  		return True
 
 	def privilege_level(self):
 		cli_output = self.ssh_command('show privilege')
-
-		if cli_output[0][:27] != 'Current privilege level is ':
+		if not cli_output[0].startswith('Current privilege level is '):
 			raise ValueError(cli_output)
-
 		return int(cli_output[0][27:])
 
 	def software_version(self):
 		if not hasattr(self, '_software_version'):
 			self._software_version = None
 			if self.can_snmp():
-				self._software_version = self._snmp_client.os_version()
+				self._software_version = self.snmp_client.os_version()
+			elif self.can_ssh():
+				cli_output = self.shell.command('show version')
+				self._software_version = ios_version(cli_output[0])
 		return self._software_version
 
 	def ssh_command(self, *args, **kwargs):
 		if not self.can_ssh():
 			raise ValueError('No SSH client')
-		if not hasattr(self, 'shell_channel'):
+		if not hasattr(self, 'shell'):
 			raise ValueError('No shell channel')
-		return self.ssh_client.shell_command(self.shell_channel, *args, **kwargs)
+		return self.shell.command(*args, **kwargs)
 
 	@staticmethod
 	def vendor():
@@ -138,37 +158,16 @@ class SNMP_Client(yandc.snmp.Client):
 		super(SNMP_Client, self).__init__(*args, **kwargs)
 
 	def os_version(self):
-		re_match = re.match(r'THING GOES HERE version (\S+) ', self.sysDescr())
-		if re_match is not None:
-			return re_match.groups(0)[0]
-
-		return None
+		return ios_version(self.sysDescr())
 
 
 class SSH_Client(yandc.ssh.Client):
-	def __init__(self, *args, **kwargs):
-		super(SSH_Client, self).__init__(*args, **kwargs)
+	pass
 
-	def on_shell_output_line(self, *args, **kwargs):
-		return super(SSH_Client, self).on_shell_output_line(*args, **kwargs).rstrip('\r').lstrip('\r')
 
-	def shell(self, prompt=None, *args):
-		generic_prompt = self.regexp_prompt(r'.+[#>]$')
-
-		if prompt is None:
-			shell_channel = super(SSH_Client, self).shell(generic_prompt, *args)
-		else:
-			shell_channel = super(SSH_Client, self).shell(prompt, *args)
-			self.shell_add_prompt(shell_channel, generic_prompt)
-
-		shell_channel.set_combine_stderr(True)
-		self.shell_command(shell_channel, 'terminal length 0')
-		self.shell_command(shell_channel, 'terminal no monitor')
-		self.shell_command(shell_channel, 'terminal width 160')
-
-		self.shell_add_prompt(shell_channel, self.regexp_prompt(r'.+\(config[^\)]*\)#$'))
-
-		return shell_channel
+class SSH_Shell(yandc.ssh.Shell):
+	def exit(self):
+		return super(SSH_Shell, self).exit('logout')
 
 
 class XR_Client(yandc.vendor_base.Client):
