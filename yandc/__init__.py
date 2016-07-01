44b9c421bcc98c4f275ce0eb0c31b3a4b69ce11a
Matt Ryan <inetuid@gmail.com>
2016-07-01 14:16:30 +0100

    Import via __init__.

--- yandc/__init__.py
+++ yandc/__init__.py
@@ -1,6 +1,18 @@
-import snmp
-import ssh
-from vendor_base import BaseClient
+__all__ = ['debug', 'EOS_Client', 'ISO_Client', 'XR_Client', 'CL_Client', 'ROS_Client']
 
-class Client_Exception(Exception):
-	pass
+import sys
+#
+from .arista import EOS_Client
+from .cisco import IOS_Client, XR_Client
+from .cumulus import CL_Client
+from .mikrotik import ROS_Client
+
+def debug(func):
+	def func_wrapper(*args, **kwargs):
+		result = func(*args, **kwargs)
+		if isinstance(result, str):
+			sys.stderr.write('DEBUG: %s(%s, %s) = [%s][%s]\n' % (func.__name__, args, kwargs, result, result.encode('hex')))
+		else:
+			sys.stderr.write('DEBUG: %s(%s, %s) = [%s]\n' % (func.__name__, args, kwargs, result))
+		return result
+	return func_wrapper
