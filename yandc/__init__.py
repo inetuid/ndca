c33a95fc737723ca35437051bc289f5f832b4b73
Matt Ryan <inetuid@gmail.com>
2016-07-03 09:09:19 +0100

    Updated debug() method.

--- yandc/__init__.py
+++ yandc/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ['debug', 'EOS_Client', 'IOS_Client', 'XR_Client', 'CL_Client', 'ROS_Client']
+__all__ = ['EOS_Client', 'IOS_Client', 'XR_Client', 'CL_Client', 'ROS_Client']
 
 import sys
 #
@@ -10,9 +10,14 @@ from .mikrotik import ROS_Client
 def debug(func):
 	def func_wrapper(*args, **kwargs):
 		result = func(*args, **kwargs)
-		if isinstance(result, str):
-			sys.stderr.write('DEBUG: %s(%s, %s) = [%s][%s]\n' % (func.__name__, args, kwargs, result, result.encode('hex')))
-		else:
-			sys.stderr.write('DEBUG: %s(%s, %s) = [%s]\n' % (func.__name__, args, kwargs, result))
+		sys.stderr.write(
+			'DEBUG: {}({}, {}) = [{}][{}]\n'.format(
+				func.__name__,
+				args,
+				kwargs,
+				result,
+				result.encode('hex') if isinstance(result, str) else ''
+			)
+		)
 		return result
 	return func_wrapper
