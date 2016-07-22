3a87586fb86e1d9d196d0e31de22d36c690459db
Matt Ryan <inetuid@gmail.com>
2016-07-22 14:12:27 +0100

    Update to split sub-packages.

--- yandc_ros/shell.py
+++ yandc_ros/shell.py
@@ -1,14 +1,16 @@
 import re
 #
-from yandc import ssh
+from yandc_ssh import Shell as BaseShell
 
 
-class Shell(ssh.Shell):
-	control_char_regexp = re.compile(r'{}\[(9999B|c)'.format(chr(0x1B)))
+class Shell(BaseShell):
+	def __init__(self, *args, **kwargs):
+		self.output_re_sub_regexp = re.compile(r'{}\[(9999B|c)'.format(chr(0x1B)))
+		super(Shell, self).__init__(*args, **kwargs)
 
 	def on_output_line(self, *args, **kwargs):
 		return re.sub(
-			Shell.control_char_regexp,
+			self.output_re_sub_regexp,
 			'',
 			super(Shell, self).on_output_line(*args, **kwargs)
 		).lstrip('\r')
