4fad40eb92d55ab3d8c2fd715c6e0222828c73a6
Matt Ryan <inetuid@gmail.com>
2016-07-22 14:09:40 +0100

    Bring in line with other modules.

--- yandc_eos/client.py
+++ yandc_eos/client.py
@@ -1,34 +1,43 @@
 import json
-import re
 #
-from .base import BaseClient
-from .. import exception, snmp, ssh
+import yandc_base as base
+import yandc_ssh as ssh
+#
+try:
+	from .snmp import Client as SNMP_Client
+except ImportError:
+	HAVE_SNMP = False
+else:
+	HAVE_SNMP = True
 
 try:
 	import pyeapi
 except ImportError:
-	have_pyeapi = False
+	HAVE_APILIB = False
 else:
-	have_pyeapi = True
+	HAVE_APILIB = True
 
 
-class Client(BaseClient):
+class Client(base.Client):
 	def __init__(self, *args, **kwargs):
 		super(Client, self).__init__(*args, **kwargs)
 
-		grouped_kwargs = self.group_kwargs('snmp_', 'ssh_', 'eapi_', **kwargs)
+		grouped_kwargs = base.Utils.group_kwargs('snmp_', 'ssh_', 'eapi_', **kwargs)
 
-		if 'snmp_' in grouped_kwargs:
+		if HAVE_SNMP and 'snmp_' in grouped_kwargs:
 			snmp_client = SNMP_Client(kwargs['host'], **grouped_kwargs['snmp_'])
 			try:
-				if not snmp_client.sysObjectID().startswith('1.3.6.1.4.1.30065.1'):
-					raise exception.ClientError('Not an Arista device')
+				sys_object_id = snmp_client.sysObjectID()
 			except snmp.SNMP_Exception:
 				pass
 			else:
+				if not self.is_arista(sys_object_id):
+					raise base.DeviceMismatchError('Not an Arista device')
 				self.snmp_client = snmp_client
 
-		if have_pyeapi:
+		self.in_configure_mode = False
+
+		if HAVE_APILIB:
 			if 'eapi_' not in grouped_kwargs:
 				grouped_kwargs['eapi_'] = {}
 			if 'host' not in grouped_kwargs['eapi_']:
@@ -54,12 +63,22 @@ class Client(BaseClient):
 			shell_prompt = ssh.ShellPrompt(ssh.ShellPrompt.regexp_prompt(r'.+[#>]$'))
 			shell_prompt.add_prompt(ssh.ShellPrompt.regexp_prompt(r'.+\(config[^\)]*\)#$'))
 
-			self.ssh_shell = ssh.Shell(self.ssh_client, shell_prompt)
-			self.ssh_shell.channel.set_combine_stderr(True)
-			self.ssh_shell.command('terminal dont-ask')
-			self.ssh_shell.command('terminal length 0')
-			self.ssh_shell.command('no terminal monitor')
-			self.ssh_shell.command('terminal width 160')
+			shell_args = {
+				'combine_stderr': True,
+				'terminal_width': 160,
+				'initial_commands': [
+					'terminal dont-ask',
+					'terminal length 0',
+					'no terminal monitor',
+					'terminal width 160',
+				],
+			}
+			self.ssh_shell = ssh.Shell(self.ssh_client, shell_prompt, optional_args=shell_args)
+#			self.ssh_shell.channel.set_combine_stderr(True)
+#			self.ssh_shell.command('terminal dont-ask')
+#			self.ssh_shell.command('terminal length 0')
+#			self.ssh_shell.command('no terminal monitor')
+#			self.ssh_shell.command('terminal width 160')
 
 	def can_eapi(self):
 		if hasattr(self, '_pyeapi_node'):
@@ -71,45 +90,48 @@ class Client(BaseClient):
 			return self.eapi_command(*args, **kwargs)
 		elif self.can_ssh():
 			return self.ssh_command(*args, **kwargs)
-		raise exception.ClientError('No valid CLI handlers')
+		raise base.ClientError('No valid CLI handlers')
 
 	def configure_via_cli(self, new_config):
+		configured_okay = True
 		if self.can_eapi():
 			try:
 				config_output = self._pyeapi_node.config(new_config)
 			except BaseException as error:
+				configured_okay = False
 				print error
-				return False
 			else:
 				for output_line in config_output:
 					if output_line != {}:
-						return False
-			return True
+						configured_okay = False
+			return configured_okay
 		elif self.can_ssh():
 			try:
 				cli_output = self.ssh_command('configure terminal')
 				if cli_output == []:
 					self.in_configure_mode = True
 				else:
-					raise exception.ClientError(cli_output[0])
+					raise base.ClientError(cli_output[0])
 
 				for config_line in new_config:
 					stripped_line = config_line.strip()
 					if stripped_line in ['end', 'exit']:
-						warnings.warn('Skipping ' + config_line)
 						continue
 
 					cli_output = self.ssh_command(stripped_line)
 					if cli_output != []:
-						raise exception.ClientError(cli_output[0])
+						configured_okay = False
+#						raise base.ClientError(cli_output[0])
+			except BaseException as error:
+				configured_okay = False
 			finally:
 				cli_output = self.ssh_command('end')
 				if cli_output == []:
 					self.in_configure_mode = False
-					return True
 				else:
-					raise exception.ClientError(cli_output[0])
-		return False
+					configured_okay = False
+#					raise base.ClientError(cli_output[0])
+		return configured_okay
 
 	def disconnect(self):
 		if self.can_eapi():
@@ -158,39 +180,21 @@ class Client(BaseClient):
 			return True
 		return False
 
-	@property
-	def in_configure_mode(self):
-		mode_mismatch = False
-		config_prompt = self.ssh_shell.last_prompt.endswith('(config)#')
-		if self._in_configure_mode:
-			if not config_prompt:
-				mode_mismatch = True
-		else:
-			if config_prompt:
-				mode_mismatch = True
-		if mode_mismatch:
-			raise exception.ClientError(
-				'Mistmatch between in_configure_mode [{}] and prompt [{}]'.format(
-					self._in_configure_mode,
-					self.ssh_shell.last_prompt
-				)
-			)
-		return self._in_configure_mode
-
 	def persist_configuration(self):
 #		if self.in_configure_mode:
 #			pass
 
 		cli_output = self.cli_command('copy running-config startup-config')
 		if cli_output[0] != 'Copy completed successfully.':
-			raise exception.ClientError(cli_output[0])
+			raise base.ClientError(cli_output[0])
 		return True
 
 	def privilege_level(self):
 		cli_output = self.cli_command('show privilege')
-		if not cli_output[0].startswith('Current privilege level is '):
-			raise exception.ClientError(cli_output)
-		return int(cli_output[0][27:])
+		partial_output = 'Current privilege level is '
+		if not cli_output[0].startswith(partial_output):
+			raise base.ClientError(cli_output)
+		return int(cli_output[0][len(partial_output):])
 
 	def software_version(self):
 		if self.can_snmp():
@@ -205,22 +209,27 @@ class Client(BaseClient):
 
 	def ssh_command(self, *args, **kwargs):
 		if not self.can_ssh():
-			raise exception.ClientError('No SSH client')
+			raise base.ClientError('No SSH client')
 		if not hasattr(self, 'ssh_shell'):
-			raise exception.ClientError('No shell channel')
-		return self.ssh_shell.command(*args, **kwargs)
+			raise base.ClientError('No shell channel')
+		shell_output = self.ssh_shell.command(*args, **kwargs)
+#		config_prompt = self.ssh_shell.last_prompt.endswith('(config)#')
+#		mode_mismatch = False
+#		if self.in_configure_mode:
+#			if not config_prompt:
+#				mode_mismatch = True
+#		else:
+#			if config_prompt:
+#				mode_mismatch = True
+#		if mode_mismatch:
+#			raise base.ClientError(
+#				'Mistmatch between in_configure_mode [{}] and prompt [{}]'.format(
+#					self.in_configure_mode,
+#					self.ssh_shell.last_prompt
+#				)
+#			)
+		return shell_output
 
 	@staticmethod
 	def vendor():
-		return 'Arista'
-
-
-class SNMP_Client(snmp.Client):
-	def os_version(self):
-		re_match = re.match(
-			r'Arista Networks EOS version (.+) running on an Arista Networks (.+)$',
-			self.sysDescr()
-		)
-		if re_match is not None:
-			return re_match.groups()[0]
-		return None
+		return ('Arista', 'EOS')
