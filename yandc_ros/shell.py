110877b11be4c084d5255de782fbee38b52f1534
Matt Ryan <inetuid@gmail.com>
2016-07-22 14:12:54 +0100

    Convert from TAB to (4) spaces.

--- yandc_ros/shell.py
+++ yandc_ros/shell.py
@@ -4,13 +4,13 @@ from yandc_ssh import Shell as BaseShell
 
 
 class Shell(BaseShell):
-	def __init__(self, *args, **kwargs):
-		self.output_re_sub_regexp = re.compile(r'{}\[(9999B|c)'.format(chr(0x1B)))
-		super(Shell, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        self.output_re_sub_regexp = re.compile(r'{}\[(9999B|c)'.format(chr(0x1B)))
+        super(Shell, self).__init__(*args, **kwargs)
 
-	def on_output_line(self, *args, **kwargs):
-		return re.sub(
-			self.output_re_sub_regexp,
-			'',
-			super(Shell, self).on_output_line(*args, **kwargs)
-		).lstrip('\r')
+    def on_output_line(self, *args, **kwargs):
+        return re.sub(
+            self.output_re_sub_regexp,
+            '',
+            super(Shell, self).on_output_line(*args, **kwargs)
+        ).lstrip('\r')
